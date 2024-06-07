7a692bfcd0dfda970914cd4c3044157dde65f9b6
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-07 15:37:18 +0200

    Update PizzaAppApplication.java

--- src/main/java/com/chumz/pizzaApp/PizzaAppApplication.java
+++ src/main/java/com/chumz/pizzaApp/PizzaAppApplication.java
@@ -1,13 +1,31 @@
 package com.chumz.pizzaApp;
 
+import com.chumz.pizzaApp.config.PizzaConfig;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PizzaAppApplication {
+@Log
+public class PizzaAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaAppApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		final PizzaConfig pizzaConfig = new PizzaConfig(
+				"tomato", "mozzarella", "thin"
+		);
+
+		log.info(
+				String.format("I want a %s crust pizza, with %s and %s sauce",
+						pizzaConfig.getCrust(),
+						pizzaConfig.getTopping(),
+						pizzaConfig.getSauce()
+				));
+	}
 }
